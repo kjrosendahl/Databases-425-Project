1e9a7126318a534395f1917b87023110c4e1c462
Noah Baxley <noah.baxley@gmail.com>
2022-04-23 14:13:44 -0500

    Adding my test sites

---
+++ TestSites/js/cart.js
@@ -0,0 +1,11 @@
+function itemSelected(index, arr) {
+    console.log(index, arr)
+    document.getElementById('selectedItem').innerHTML = arr[index];
+}
+
+
+/**
+ * Need to add categories to items and be able to limit items to those
+ * As well as brands
+ * 
+ */
