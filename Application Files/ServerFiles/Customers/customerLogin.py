ec679c09fc04f084c4d74b9e74f48ed51f9c9477
Kaylee Rosendahl <98189787+kjrosendahl@users.noreply.github.com>
2022-04-26 21:08:40 -0500

    Tested, success!

--- Application Files/ServerFiles/Customers/customerLogin.py
+++ Application Files/ServerFiles/Customers/customerLogin.py
@@ -1,3 +1,16 @@
 def loginVerification(email, pa):
-    # Check password matches and email matches
-    return boolean cid
+    try: 
+        sql = """select CID from OnlineAcc where email = (:email) and password = (:password)"""
+        
+        # query result 
+        cursor.execute(sql, [email, pa])
+
+        x = cursor.fetchall()
+        
+        # if account exists, return CID
+        if x: 
+            return (True, x[0][0])
+        else: 
+            return (False, 'null')
+    except DBFailure: 
+        print('Something went wrong. Please try again.')
