073affc2478534ed7272af1df206a387fb77d0fa
Kaylee Rosendahl <98189787+kjrosendahl@users.noreply.github.com>
2022-04-29 01:59:24 -0500

    Create requestAddress.py

---
+++ Application Files/ServerFiles/Customers/PaymentPage/requestAddress.py
@@ -0,0 +1,13 @@
+# customer may request their address info. Returns T/F, tuple of info or null, message
+def requestAddress(CID):
+    try: 
+        sql = """select * from CustAddress where CID = :CID"""
+        cursor.execute(sql, [CID])
+        x = cursor.fetchall()
+        
+        if(x):
+            return(True, x[0], "Address found")
+        else: 
+            return(False, "null", "Address not found.")
+    except: 
+        ("Something went wrong. Try again.")
