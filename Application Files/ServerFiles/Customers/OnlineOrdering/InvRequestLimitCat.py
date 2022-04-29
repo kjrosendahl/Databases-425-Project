e36b1380b7a54435c6159504981166ab0f7869d8
Kaylee Rosendahl <98189787+kjrosendahl@users.noreply.github.com>
2022-04-28 22:43:24 -0500

    Update InvRequestLimitCat.py

--- Application Files/ServerFiles/Customers/OnlineOrdering/InvRequestLimitCat.py
+++ Application Files/ServerFiles/Customers/OnlineOrdering/InvRequestLimitCat.py
@@ -1,7 +1,7 @@
 # returns a list of the products, product info when given an inventory ID, limit by the category ID
 def InvRequestLimCat(ID, CatID):
     try: 
-        sql = """select PID, ProductName, Price, Quantity, BrandName, CategoryName from ProdInv natural join 
+        sql = """select PID, ProductName, Price, Quantity, BrandName from ProdInv natural join 
                 Products natural join Brands natural join Categories where InvID = :ID and CategoryID = :CatID"""
         cursor.execute(sql, [ID, CatID])
         x = cursor.fetchall()
