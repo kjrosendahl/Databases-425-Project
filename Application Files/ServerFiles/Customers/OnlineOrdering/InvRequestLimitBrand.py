bf10beacfa5f13bea6a74be2881a4577d98f4f53
Kaylee Rosendahl <98189787+kjrosendahl@users.noreply.github.com>
2022-04-28 22:42:08 -0500

    Update InvRequestLimitBrand.py

--- Application Files/ServerFiles/Customers/OnlineOrdering/InvRequestLimitBrand.py
+++ Application Files/ServerFiles/Customers/OnlineOrdering/InvRequestLimitBrand.py
@@ -1,7 +1,7 @@
 # returns a list of the products, product info when given an inventory ID, limit by the brand ID
 def InvRequestLimBrand(ID, BID):
     try: 
-        sql = """select PID, ProductName, Price, Quantity, BrandName, CategoryName from ProdInv natural join 
+        sql = """select PID, ProductName, Price, Quantity, CategoryName from ProdInv natural join 
                 Products natural join Brands natural join Categories where InvID = :ID and BrandID = :BID"""
         cursor.execute(sql, [ID, BID])
         x = cursor.fetchall()
