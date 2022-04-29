6a33d0a58230e9b4221ad8f257bc281d22cb3b24
Kaylee Rosendahl <98189787+kjrosendahl@users.noreply.github.com>
2022-04-28 21:37:51 -0500

    Create customerCreateRecord.py

---
+++ Application Files/ServerFiles/Customers/customerCreateRecord.py
@@ -0,0 +1,14 @@
+# Customer: create a new record
+def newCustomerRecord(Name): 
+    try: 
+        sql = """select max(CID) from Customer"""
+        cursor.execute(sql)  
+        x = cursor.fetchall()
+        CID = x[0][0] + 1
+        
+        sql = """insert into Customer values (:CID, :Name, :Frequent)"""
+        cursor.execute(sql, [CID, Name, "False"])
+        
+        return(True, CID)
+    except:
+        return(False, "null")
