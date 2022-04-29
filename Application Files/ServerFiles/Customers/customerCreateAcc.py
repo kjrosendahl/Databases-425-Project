66a921bc1f15bfe91c3321331df10dda0e8d2435
Kaylee Rosendahl <98189787+kjrosendahl@users.noreply.github.com>
2022-04-28 23:39:47 -0500

    Update customerCreateAcc.py

--- Application Files/ServerFiles/Customers/customerCreateAcc.py
+++ Application Files/ServerFiles/Customers/customerCreateAcc.py
@@ -1,20 +1,16 @@
-# Customer: create account
-def createAccount(CID, email, password, street, city, state, zipcode): 
+# Customer: create account (requires Customer ID, email/password)
+def createAccount(CID, email, password):
     try: 
         try: 
             sql = """insert into OnlineAcc values (:CID, :email, :password)"""
             cursor.execute(sql, [CID, email, password])
-        except:
-            print('Something went wrong. Please enter a valid and unique email and password.')
-
-        try: 
-            sql = """insert into CustAddress values (:CID, :street, :city, :state, :zipcode)"""
-            cursor.execute(sql, [CID, street, city, state, zipcode])
-        except:
-            print('Something went wrong. Please enter a valid address.')
+            
+            connection.commit()
+            return(True, CID, "Account created successfully.")
+            
+        except: 
+            return(False, CID, "Account unsuccessful. Please enter a unique email. Passwords must be at least 6 characters.")
         
-        connection.commit()
-        return(True, CID, "Account created successfully.")
     except: 
         return(False, "null", "Something went wrong. Please try again.")
         
