376acbdbaeff5dc8f0e872146d6804bb6130e53a
Kaylee Rosendahl <98189787+kjrosendahl@users.noreply.github.com>
2022-04-30 01:39:06 -0500

    Create changePassword.py

---
+++ Application Files/ServerFiles/Customers/ViewModifyAcc/changePassword.py
@@ -0,0 +1,18 @@
+# customers may change password 
+# returns boolean, message 
+# only fulfills request if old password is entered correctly 
+def changePassword(CID, oldPass, newPass): 
+    try: 
+        sql = """select Password from OnlineAcc where CID = :CID"""
+        x = (cursor.execute(sql, [CID])).fetchall()
+        if x[0][0] != oldPass: 
+            return(False, 'Please enter the correct current password.')
+        else: 
+            sql = """update OnlineAcc set Password = :newPass where CID = :CID"""
+            cursor.execute(sql, [newPass, CID])
+
+            connection.commit() 
+
+            return(True, 'Password updated.')
+    except: 
+        return(False, 'Something went wrong. Please enter a valid password.')
