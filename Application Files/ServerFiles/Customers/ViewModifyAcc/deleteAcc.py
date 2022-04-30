7eac3e4c2222051c5aa1acf8f592312bbcc545e6
Kaylee Rosendahl <98189787+kjrosendahl@users.noreply.github.com>
2022-04-30 01:39:20 -0500

    Create deleteAcc.py

---
+++ Application Files/ServerFiles/Customers/ViewModifyAcc/deleteAcc.py
@@ -0,0 +1,17 @@
+# customers may delete their account 
+# deletes their Account, card info, and address
+# returns boolean, message
+def deleteAcc(CID): 
+    try: 
+        sql = """delete from OnlineAcc where CID = :CID"""
+        cursor.execute(sql, [CID])
+        sql = """delete from Credit where CID = :CID"""
+        cursor.execute(sql, [CID])
+        sql = """delete from CustAddress where CID = :CID"""
+        cursor.execute(sql, [CID])
+        
+        connection.commit()
+        
+        return (True, 'Account deleted.')
+    except: 
+        return(False, 'Something went wrong. Please try again.')
