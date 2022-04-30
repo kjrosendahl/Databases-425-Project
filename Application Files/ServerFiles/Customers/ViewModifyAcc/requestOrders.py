8309114e13f71eadb1613792ac37680c210eb3d8
Kaylee Rosendahl <98189787+kjrosendahl@users.noreply.github.com>
2022-04-30 16:32:44 -0500

    Update requestOrders.py

--- Application Files/ServerFiles/Customers/ViewModifyAcc/requestOrders.py
+++ Application Files/ServerFiles/Customers/ViewModifyAcc/requestOrders.py
@@ -1,6 +1,6 @@
-# frequent customers may request their previous orders. Returns a list of tuples of the broad order details
+# frequent customers may request their previous orders. Rereturns a list of tuples of the broad order details 
 def requestOrders(email): 
-    sql = """select OrderID, day, month, year, total, trackNo, Status from Orders natural join OnlineAcc 
-            natural join OrderShip where email = :email"""
+    sql = """select OrderID, day, month, year, total, status, typeOrder from Orders natural join OnlineAcc 
+            where email = :email"""
     x = (cursor.execute(sql, [email])).fetchall()
-    return (x)
+    return(x)
