6da71b2ab32f18c2eda5bdb3f80f0a28cae6700b
Noah Baxley <noah.baxley@gmail.com>
2022-05-05 00:35:18 -0500

    Changed from d/m/y to date object

--- Application Files/ServerFilesCustomer/ViewModifyAcc/requestOrders.py
+++ Application Files/ServerFilesCustomer/ViewModifyAcc/requestOrders.py
@@ -1,7 +1,6 @@
 # frequent customers may request their previous orders. Rereturns a list of tuples of the broad order details 
 def requestOrders(connection, email: str): 
     cursor = connection.cursor()    
-    sql = """select OrderID, day, month, year, total, status, typeOrder from Orders natural join OnlineAcc 
-            where email = :email"""
+    sql = """select OrderID, Odate, total, status from Orders natural join OnlineAcc  where email = :email"""
     x = (cursor.execute(sql, [email])).fetchall()
     return(x)
